2ef007b746cfad34d88275b9d37c39adcdc9711c
MdAslam54321 <md.aslam54321@gmail.com>
2023-06-05 16:07:31 +0530

    Welcome to master added 2

--- HelloWorld/src/org/studyeasy/Dev.java
+++ HelloWorld/src/org/studyeasy/Dev.java
@@ -1,5 +1,6 @@
 public class Dev{
     public static void main(String[] args) {
         System.out.println("Welcome to master branch");
+        System.out.println("Welcome to Mater branch 2");
     }
 }
