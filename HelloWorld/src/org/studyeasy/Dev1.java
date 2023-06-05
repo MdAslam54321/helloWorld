1d1bd07ce86bd7161ed43e242e24acf21090b568
MdAslam54321 <md.aslam54321@gmail.com>
2023-06-05 17:53:15 +0530

    Welcome to dev added2.

--- HelloWorld/src/org/studyeasy/Dev1.java
+++ HelloWorld/src/org/studyeasy/Dev1.java
@@ -5,6 +5,7 @@ public class Dev1 {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         System.out.println("Welcome to Dev branch1");
+        System.out.println("Welcome to Dev branch2");
     }
 
 }
